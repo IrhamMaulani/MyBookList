7dd52df19943b536fce2a16a886526f4c0442243
Irham Maulani <irhammaulani@gmail.com>
2019-05-12 00:24:15 +0800

    add icon and and bottomnavview

---
+++ app/src/main/java/com/example/rakbuku/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.rakbuku;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.rakbuku.fragment.HomeFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        loadFragment(new HomeFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setItemIconTintList(null);
+
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                fragment = new HomeFragment();
+
+            case R.id.action_shelf:
+
+            case R.id.action_profile:
+
+        }
+        return true;
+    }
+}
